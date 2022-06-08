01ee62d74bc1f83e18f894e31c63c5c1787d2942
Yasin Ogras <gs_yasin@hotmail.fr>
2022-06-08 08:57:30 +0200

    live server fixed my cross origin problem

---
+++ composantFonctionnel.js
@@ -0,0 +1,11 @@
+// console.log(React)
+
+// function MyComponent() {
+//   return <div>Hello OpenClassrooms 👋</div>;
+// }
+// ReactDOM.render(<MyComponent />, document.getElementById("react-goes-here"));
+
+function MyComponent() {
+  return <div>Mon élément remplace le contenu précédent </div>;
+}
+ReactDOM.render(<MyComponent />, document.getElementById("root"));
